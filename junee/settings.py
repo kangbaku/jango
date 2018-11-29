f7f01a09653897ae98e990241c9c183fe079da79
kangbaku <kangbaku@gmail.com>
2018-11-29 11:35:46 +0800

    edited settings.py

--- junee/settings.py
+++ junee/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c29f(ap$ou(91l7e5w+a3ns8q)9q4rh&=t)c6%pu^y8*sv5e&*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
